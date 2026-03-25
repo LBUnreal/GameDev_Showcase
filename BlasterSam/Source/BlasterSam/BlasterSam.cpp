0eef5521ada9823abb884d1c795f334fae5013c7
STELLA2\lukas <lukasbayard@hotmail.com>
2026-03-24 20:03:50 -0400

    Revert "Blaster Sam - Task - Project Setup - PHASE 1 - Added Assets, Classes and created Blueprints for the Tank and Turret classes. Still need to create blueprints for the Shooter Character, as well as, add the AI behavior Trees and testing in a level prototype. Excluded Paragon Wraith content since it is available for free on the Fab Store, and ShooterGame assets which should be downloaded through GameDevTV"
    This reverts commit cf260cc38a7d422fe2def7c17beae5f13daaacd3.

--- BlasterSam/Source/BlasterSam/BlasterSam.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BlasterSam.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlasterSam, "BlasterSam" );
-
-DEFINE_LOG_CATEGORY(LogBlasterSam)
--- BlasterSam/Source/BlasterSam/BlasterSamGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BlasterSamGameMode.h"
-
-ABlasterSamGameMode::ABlasterSamGameMode()
-{
-	// stub
-}
